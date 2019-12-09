9f0ef83bd930d2aa877cf50beef1d8756e97a342
Daniel Santiago Tenjo Leal <ds.tenjo@uniandes.edu.co>
2019-12-09 18:41:17 -0500

    No me toque el cuello

--- ProyectoDalgo/src/ProblemaA.java
+++ ProyectoDalgo/src/ProblemaA.java
@@ -4,14 +4,59 @@ import java.io.InputStreamReader;
 
 public class ProblemaA {
 	
+	
+	static String longestRepeatedSubstring(String str, int n) { 
+		
+        int LCSRe[][] = new int[n + 1][n + 1]; 
+  
+        String res = ""; // To store result  
+        int res_length = 0; // To store length of result  
+  
+        // building table in bottom-up manner  
+        int i, index = 0; 
+        for (i = 1; i <= n; i++) { 
+            for (int j = i + 1; j <= n; j++) { 
+                // (j-i) > LCSRe[i-1][j-1] to remove  
+                // overlapping  
+                if (str.charAt(i - 1) == str.charAt(j - 1) 
+                        && LCSRe[i - 1][j - 1] < (j - i)) { 
+                    LCSRe[i][j] = LCSRe[i - 1][j - 1] + 1; 
+  
+                    // updating maximum length of the  
+                    // substring and updating the finishing  
+                    // index of the suffix  
+                    if (LCSRe[i][j] > res_length) { 
+                        res_length = LCSRe[i][j]; 
+                        index = Math.max(i, index); 
+                    } 
+                } else { 
+                    LCSRe[i][j] = 0; 
+                } 
+            } 
+        } 
+  
+        // If we have non-empty result, then insert all  
+        // characters from first character to last  
+        // character of String  
+        if (res_length > 0) { 
+            for (i = index - res_length + 1; i <= index; i++) { 
+                res += str.charAt(i - 1); 
+            } 
+        } 
+  
+        return res; 
+    } 
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String size, datos[];
 		size = br.readLine();
 		int rta = 0;
+		String info="";
 		while(!size.equals("0"))
 		{
-			datos = br.readLine().split(" ");
+			info = br.readLine();
+			datos=info.split(" ");
 			rta=0;
 			int i = 0;
 			int j = datos.length/2;
@@ -32,6 +77,7 @@ public class ProblemaA {
 			size = br.readLine();
 			
 		}
+		longestRepeatedSubstring(info, Integer.parseInt(size));
 		
 	}
 
